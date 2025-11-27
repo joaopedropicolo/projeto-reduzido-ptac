f4aa0699e00a642260fc5e4aee5dcfe954269f51
renansenamtb <renansenazzz@gmail.com>
2025-11-27 13:25:53 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
   return (
     <footer className="footer">
       <div className="container">
-        <div className="note">Telefone: (67) 99609-0360 - Rua Luane Sena Picolo 666 - Rio De Janeiro - MS</div>
+        <div className="note">Telefone: (67) 99609-0360 - Rua Luane Sena Picolo 666 - Rio De Janeiro - RJ</div>
         <div style={{marginTop:6}}>&copy; {new Date().getFullYear()} Trio Parada Dura </div>
       </div>
     </footer>
