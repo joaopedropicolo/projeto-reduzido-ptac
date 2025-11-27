e973bedfeb4185670f9d0ebe06b81bc774e47687
renansenamtb <renansenazzz@gmail.com>
2025-11-27 13:24:57 -0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ export default function Footer(){
   return (
     <footer className="footer">
       <div className="container">
-        <div className="note">xxxxxx (99) 99999-9999 - Rua Exemplo, 123 - Cidade - UF</div>
-        <div style={{marginTop:6}}>&copy; {new Date().getFullYear()} Reservas de Mesas</div>
+        <div className="note">Telefone: (67) 99609-0360 - Rua Luane Sena Picolo 666 - Rio De Janeiro - MS</div>
+        <div style={{marginTop:6}}>&copy; {new Date().getFullYear()} Trio Parada Dura </div>
       </div>
     </footer>
   );
