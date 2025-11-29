87a0f131528e77fa44cbe0ec8d4d6d7db5a7b58e
Renan Sena <renansenazzz@gmail.com>
2025-11-28 18:23:13 -0800

    img cardapio

--- src/pages/Cardapio.jsx
+++ src/pages/Cardapio.jsx
@@ -7,9 +7,23 @@ export default function Cardapio(){
   return (
     <div>
       <h2>Cardápio</h2>
+
       <div className="grid">
         {menu.map(item => (
           <div key={item.id} className="menu-item card">
+
+            <img 
+              src={item.img} 
+              alt={item.name} 
+              className="food-img"
+              style={{
+                width: "100%",
+                height: "180px",
+                objectFit: "cover",
+                borderRadius: "10px"
+              }}
+            />
+
             <h3>{item.name}</h3>
             <div className="small">{item.desc}</div>
             <div style={{marginTop:8, fontWeight:700}}>{item.price}</div>
